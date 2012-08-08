a527fab6fef86f291ae8c51704d0be3c9c609874
m.mcgill <m.mcgill@techsmith.com>
2012-08-08 08:27:04 -0400

    adapt CloudStorageAccount to ICloudStorageAccount

---
+++ src/DataModel.Core/CloudStorageAccountAdapter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.WindowsAzure;
+
+namespace TechSmith.CloudServices.DataModel.Core
+{
+   /// <summary>
+   /// Adapts Microsoft.WindowsAzure.CloudStorageAccount to the ICloudStorageAccount interface.
+   /// </summary>
+   public class CloudStorageAccountAdapter : ICloudStorageAccount
+   {
+      private readonly CloudStorageAccount _account;
+
+      public CloudStorageAccountAdapter( CloudStorageAccount account )
+      {
+         _account = account;
+      }
+
+      public string TableEndpoint
+      {
+         get
+         {
+            return _account.TableEndpoint.AbsoluteUri;
+         }
+      }
+
+      public StorageCredentials Credentials
+      {
+         get
+         {
+            return _account.Credentials;
+         }
+      }
+   }
+}
