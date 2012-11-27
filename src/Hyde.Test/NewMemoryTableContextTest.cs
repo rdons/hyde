8a848e5980ff430a059d712e32610e591a7f0833
m.mcgill <m.mcgill@techsmith.com>
2012-11-27 15:01:51 -0500

    new MemoryTableContext: add/get item
    The implementation of the existing MemoryTableContext
    has, over time, become unnecessarily complicated.
    That complexity will make it difficult, in the future,
    to add support for Entity Group Transactions.
    
    With these things in mind, we start a fresh implementation
    of ITableContext. This commit starts with adding and
    retrieving individual items.

---
+++ src/Hyde.Test/NewMemoryTableContextTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechSmith.Hyde.Common;
+using TechSmith.Hyde.Table.Memory;
+
+namespace TechSmith.Hyde.Test
+{
+   [TestClass]
+   public class NewMemoryTableContextTest
+   {
+      private NewMemoryTableContext _context;
+
+      [TestInitialize]
+      public void Initialize()
+      {
+         NewMemoryTableContext.ResetAllTables();
+         _context = new NewMemoryTableContext();
+      }
+
+      [TestMethod]
+      [ExpectedException( typeof( EntityDoesNotExistException ) )]
+      public void GetItem_ItemNotFound_ThrowsEntityNotFoundException()
+      {
+         _context.GetItem<DecoratedItem>( "foo", "pk", "rk" );
+      }
+
+      [TestMethod]
+      public void GetItem_ItemAddedButSaveNotCalled_ThrowsEntityDoesNotExistException()
+      {
+         _context.AddNewItem( "table", new DecoratedItem { Id = "abc", Name = "123", Age = 50 }, "abc", "123" );
+         try
+         {
+            _context.GetItem<DecoratedItem>( "table", "abc", "123" );
+            Assert.Fail( "Should have thrown exception" );
+         }
+         catch ( EntityDoesNotExistException )
+         {
+         }
+      }
+
+      [TestMethod]
+      public void Save_ItemAdded_GetItemReturnsTheItem()
+      {
+         var addedItem = new DecoratedItem { Id = "abc", Name = "123", Age = 50 };
+         _context.AddNewItem( "table", addedItem, "abc", "123" );
+         _context.Save();
+
+         var returnedItem = _context.GetItem<DecoratedItem>( "table", "abc", "123" );
+         Assert.AreEqual( addedItem.Id, returnedItem.Id );
+         Assert.AreEqual( addedItem.Name, returnedItem.Name );
+         Assert.AreEqual( addedItem.Age, returnedItem.Age );
+      }
+
+      [TestMethod]
+      public void GetItem_ItemAddedAndSavedWithDifferentContext_ReturnsItem()
+      {
+         var addedItem = new DecoratedItem { Id = "abc", Name = "123", Age = 50 };
+         _context.AddNewItem( "table", addedItem, "abc", "123" );
+         _context.Save();
+
+         var returnedItem = new NewMemoryTableContext().GetItem<DecoratedItem>( "table", "abc", "123" );
+         Assert.AreEqual( addedItem.Id, returnedItem.Id );
+         Assert.AreEqual( addedItem.Name, returnedItem.Name );
+         Assert.AreEqual( addedItem.Age, returnedItem.Age );
+      }
+
+      [TestMethod]
+      public void GetItem_ItemAddedToDifferentTable_ThrowsEntityDoesNotExistException()
+      {
+         var addedItem = new DecoratedItem { Id = "abc", Name = "123", Age = 50 };
+         _context.AddNewItem( "table", addedItem, "abc", "123" );
+         _context.Save();
+
+         try
+         {
+            _context.GetItem<DecoratedItem>( "diffTable", "abc", "123" );
+            Assert.Fail( "Should have thrown exception" );
+         }
+         catch ( EntityDoesNotExistException )
+         {
+         }
+      }
+   }
+}
---
+++ src/Hyde/Table/Memory/NewMemoryTableContext.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using TechSmith.Hyde.Common;
+using TechSmith.Hyde.Table.Azure;
+
+namespace TechSmith.Hyde.Table.Memory
+{
+   internal class NewMemoryTableContext : ITableContext
+   {
+      private class Table
+      {
+         private readonly Dictionary<Tuple<string, string>, GenericTableEntity> _entities = new Dictionary<Tuple<string, string>, GenericTableEntity>();
+
+         public GenericTableEntity Get( string partitionKey, string rowKey )
+         {
+            var key = new Tuple<string, string>( partitionKey, rowKey );
+            if ( ! _entities.ContainsKey( key ) )
+            {
+               throw new EntityDoesNotExistException();
+            }
+            return _entities[key];
+         }
+
+         public void Add( GenericTableEntity entity )
+         {
+            _entities[new Tuple<string, string>( entity.PartitionKey, entity.RowKey )] = entity;
+         }
+      }
+
+      private class StorageAccount
+      {
+         private readonly Dictionary<string, Table> _tables = new Dictionary<string, Table>();
+
+         public Table GetTable( string tableName )
+         {
+            if ( !_tables.ContainsKey( tableName ) )
+            {
+               _tables.Add( tableName, new Table() );
+            }
+            return _tables[tableName];
+         }
+      }
+
+      private static StorageAccount _tables = new StorageAccount();
+
+      private readonly Queue<Action<StorageAccount>> _pendingActions = new Queue<Action<StorageAccount>>();
+
+      public static void ResetAllTables()
+      {
+         _tables = new StorageAccount();
+      }
+
+      public T GetItem<T>( string tableName, string partitionKey, string rowKey ) where T : new()
+      {
+         return _tables.GetTable( tableName ).Get( partitionKey, rowKey ).ConvertTo<T>();
+      }
+
+      public IEnumerable<T> GetCollection<T>( string tableName ) where T : new()
+      {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<T> GetCollection<T>( string tableName, string partitionKey ) where T : new()
+      {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<T> GetRangeByPartitionKey<T>( string tableName, string partitionKeyLow, string partitionKeyHigh ) where T : new()
+      {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<T> GetRangeByRowKey<T>( string tableName, string partitionKey, string rowKeyLow, string rowKeyHigh ) where T : new()
+      {
+         throw new NotImplementedException();
+      }
+
+      public dynamic GetItem( string tableName, string partitionKey, string rowKey )
+      {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<dynamic> GetCollection( string tableName )
+      {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<dynamic> GetCollection( string tableName, string partitionKey )
+      {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<dynamic> GetRangeByPartitionKey( string tableName, string partitionKeyLow, string partitionKeyHigh )
+      {
+         throw new NotImplementedException();
+      }
+
+      public IEnumerable<dynamic> GetRangeByRowKey( string tableName, string partitionKey, string rowKeyLow, string rowKeyHigh )
+      {
+         throw new NotImplementedException();
+      }
+
+      public void AddNewItem( string tableName, dynamic itemToAdd, string partitionKey, string rowKey )
+      {
+         var entity = GenericTableEntity.HydrateFrom( itemToAdd, partitionKey, rowKey );
+         _pendingActions.Enqueue( tables => tables.GetTable( tableName ).Add( entity ) );
+      }
+
+      public void Upsert( string tableName, dynamic itemToUpsert, string partitionKey, string rowKey )
+      {
+         throw new NotImplementedException();
+      }
+
+      public void Update( string tableName, dynamic item, string partitionKey, string rowKey )
+      {
+         throw new NotImplementedException();
+      }
+
+      public void DeleteItem( string tableName, string partitionKey, string rowKey )
+      {
+         throw new NotImplementedException();
+      }
+
+      public void DeleteCollection( string tableName, string partitionKey )
+      {
+         throw new NotImplementedException();
+      }
+
+      public void Save()
+      {
+         foreach ( var action in _pendingActions )
+         {
+            action( _tables );
+         }
+      }
+
+      public IEnumerable<T> GetRange<T>( string tableName, string partitionKeyLow, string partitionKeyHigh ) where T : new()
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
