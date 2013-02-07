d79c09c9dcc0ae2ee0cae90cd33d0f18705f75dc
m.mcgill <m.mcgill@techsmith.com>
2013-02-06 22:40:49 -0500

    introduce TableItem
    The TableItem class holds the hydratable properties of a hydrated or
    dehydratd object, along with its partition and row keys. We'll build up
    its functionality in stages, and then work it into TableStorageProvider,
    ITableContext, and its implementations.
    
    We start with support for constructing a TableItem from an object,
    capturing its hydratable properties via reflection. A flag controls
    behavior in the case where hydratable properties have names reserved by
    Table Storage.

---
+++ src/Hyde.Test/TableItemTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechSmith.Hyde.Common;
+using TechSmith.Hyde.Common.DataAnnotations;
+using TechSmith.Hyde.Table;
+
+namespace TechSmith.Hyde.Test
+{
+   [TestClass]
+   public class TableItemTest
+   {
+      [TestMethod]
+      public void Create_ValidEntity_PropertiesSetCorrectly()
+      {
+         var item = TableItem.Create( new SimpleDataItem { FirstType = "Joe", SecondType = 34 }, "pk", "rk", true );
+
+         Assert.AreEqual( 3, item.Properties.Count );
+         Assert.AreEqual( "Joe", item.Properties["FirstType"].Item1 );
+         Assert.AreEqual( 34, item.Properties["SecondType"].Item1 );
+         Assert.IsNull( item.Properties["UriTypeProperty"].Item1 );
+      }
+
+      [TestMethod]
+      public void CreateAndThrowOnReservedProperty_KeysProvidedAndNoReservedProperties_KeysSetCorrectly()
+      {
+         var item = TableItem.Create( new SimpleDataItem { FirstType = "Joe", SecondType = 34 }, "pk", "rk", true );
+
+         Assert.AreEqual( "pk", item.PartitionKey );
+         Assert.AreEqual( "rk", item.RowKey );
+      }
+
+      [TestMethod]
+      [ExpectedException( typeof( InvalidEntityException ) )]
+      public void CreateAndThrowOnReservedProperty_KeysProvidedAndHasReservedProperty_ThrowsInvalidEntityException()
+      {
+         TableItem.Create( new ClassWithTimestamp { Name = "Joe", Timestamp = DateTime.Now }, "pk", "rk", true );
+      }
+
+      [TestMethod]
+      public void CreateAndIgnoreReservedProperty_KeysProvidedAndHasReservedProperty_IgnoresReservedProperty()
+      {
+         var item = TableItem.Create( new ClassWithUndecoratedPartitionKey { Name = "Joe", PartitionKey = "should be ignored" }, "pk", "rk", false );
+
+         Assert.AreEqual( 1, item.Properties.Count );
+         Assert.AreEqual( "Joe", item.Properties["Name"].Item1 );
+         Assert.AreEqual( "pk", item.PartitionKey );
+      }
+   }
+
+   class ClassWithTimestamp
+   {
+      public string Name { get; set; }
+
+      public DateTime Timestamp { get; set; }
+   }
+
+   class ClassWithUndecoratedPartitionKey
+   {
+      public string Name { get; set; }
+
+      public string PartitionKey { get; set; }
+   }
+}
---
+++ src/Hyde/Table/TableItem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TechSmith.Hyde.Common;
+
+namespace TechSmith.Hyde.Table
+{
+   public class TableItem
+   {
+      public string PartitionKey
+      {
+         get;
+         private set;
+      }
+
+      public string RowKey
+      {
+         get;
+         private set;
+      }
+
+      public Dictionary<string, Tuple<object, Type>> Properties
+      {
+         get;
+         private set;
+      }
+
+      private TableItem( Dictionary<string, Tuple<object, Type>> properties, bool throwOnReservedProperties )
+      {
+         Properties = new Dictionary<string, Tuple<object, Type>>();
+         foreach ( string propertyName in properties.Keys )
+         {
+            if ( !TableConstants.ReservedPropertyNames.Contains( propertyName ) )
+            {
+               Properties[propertyName] = properties[propertyName];
+            }
+            else if ( throwOnReservedProperties )
+            {
+               throw new InvalidEntityException( string.Format( "Reserved property name {0}", propertyName ) );
+            }
+         }
+      }
+
+      public static TableItem Create( object entity, string partitionKey, string rowKey, bool throwOnReservedPropertyName )
+      {
+         var properties = new Dictionary<string, Tuple<object, Type>>();
+         foreach ( var property in entity.GetType().GetProperties().Where( p => p.ShouldSerialize() ) )
+         {
+            properties[property.Name] = new Tuple<object, Type>( property.GetValue( entity, null ), property.PropertyType );
+         }
+         var item = new TableItem( properties, throwOnReservedPropertyName );
+         item.PartitionKey = partitionKey;
+         item.RowKey = rowKey;
+         return item;
+      }
+   }
+}
