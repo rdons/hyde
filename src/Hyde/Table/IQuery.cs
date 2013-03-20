5e5e23e568315a3fb7074e049331678fc626a25e
m.mcgill <m.mcgill@techsmith.com>
2013-03-20 10:45:51 -0400

    sketch out a fluent query API
    IFilterable<T> and companion interfaces provide a fluent API for
    expressing table storage queries on partition and row keys.
    
    AbstractQuery implements IFilterable and constructs a QueryDescriptor
    that describes a query to be performed.

--- src/Hyde/Table/IQuery.cs
+++ src/Hyde/Table/IQuery.cs
@@ -2,8 +2,36 @@
 
 namespace TechSmith.Hyde.Table
 {
-   public interface IQuery<T> : IEnumerable<T> where T : new()
+   public interface IFilterable<T> : IPkLowBoundedFilterable<T>
    {
-      IQuery<T> Top( int count );
+      IBoundChoice<IPkLowBoundedFilterable<T>> PartitionKeyFrom( string value );
+      IRkFilterable<T> PartitionKeyEquals( string value );
+   }
+
+   public interface IBoundChoice<T>
+   {
+      T Exclusive();
+      T Inclusive();
+   }
+
+   public interface IPkLowBoundedFilterable<T> : IRkFilterable<T>
+   {
+      IBoundChoice<IRkFilterable<T>> PartitionKeyTo( string value );
+   }
+
+   public interface IRkFilterable<T> :IRkLowBoundedFilterable<T>
+   {
+      IBoundChoice<IRkLowBoundedFilterable<T>> RowKeyFrom( string value );
+      IQuery<T> RowKeyEquals( string value );
+   }
+
+   public interface IRkLowBoundedFilterable<T> : IQuery<T>
+   {
+      IBoundChoice<IQuery<T>> RowKeyTo( string value );
+   }
+
+   public interface IQuery<T> : IEnumerable<T>
+   {
+      IEnumerable<T> Top( int count );
    }
 }
---
+++ src/Hyde/Table/QueryDescriptor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TechSmith.Hyde.Table
+{
+   public struct KeyBound
+   {
+      public string Value;
+      public bool IsInclusive;
+   }
+
+   public struct KeyRange
+   {
+      public KeyBound? Lower;
+      public KeyBound? Upper;
+
+      public bool IsSingleValue()
+      {
+         return Lower.HasValue && Upper.HasValue &&
+                Lower.Value.Value == Upper.Value.Value &&
+                Lower.Value.IsInclusive && Upper.Value.IsInclusive;
+      }
+
+      public string SingleValue()
+      {
+         if ( !IsSingleValue() )
+            throw new InvalidOperationException( "range does not have identical, inclusive upper and lower bounds");
+         return Lower.Value.Value;
+      }
+   }
+
+   public struct QueryDescriptor
+   {
+      public KeyRange RowKeyRange;
+
+      public KeyRange PartitionKeyRange;
+
+      public int? TopCount;
+   }
+}
