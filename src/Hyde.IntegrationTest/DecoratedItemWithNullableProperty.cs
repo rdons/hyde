26036c2d48fd679ab2a7ccf26297fb1ddae24feb
J.Kling <J.Kling@techsmith.com>
2013-03-28 13:11:13 -0400

    Add integration test to assert azure doesnt handle nulls

---
+++ src/Hyde.IntegrationTest/DecoratedItemWithNullableProperty.cs
@@ -0,0 +1,30 @@
+﻿using TechSmith.Hyde.Common.DataAnnotations;
+
+namespace TechSmith.Hyde.IntegrationTest
+{
+   /// <summary>
+   /// Class with decorated partition and row key properties, for testing purposes.
+   /// </summary>
+   internal class DecoratedItemWithNullableProperty
+   {
+      [PartitionKey]
+      public string Id
+      {
+         get;
+         set;
+      }
+
+      [RowKey]
+      public string Name
+      {
+         get;
+         set;
+      }
+
+      public string Description
+      {
+         get;
+         set;
+      }
+   }
+}
