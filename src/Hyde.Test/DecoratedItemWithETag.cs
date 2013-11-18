766e24b46b1cd4fd3fa3fb16dc7ff46f9e868be3
Eric Beyer <e.beyer@techsmith.com>
2013-11-18 13:37:02 -0500

    Added an ETag Attribute to decorate a class property to indicate it should retreive the ETag. Added this property to the TableItem and assign it when creating from a static type.

---
+++ src/Hyde.Test/DecoratedItemWithETag.cs
@@ -0,0 +1,37 @@
+﻿using TechSmith.Hyde.Common.DataAnnotations;
+
+namespace TechSmith.Hyde.Test
+{
+   /// <summary>
+   /// Class with decorated partition and row key properties, for testing purposes.
+   /// </summary>
+   class DecoratedItemWithETag
+   {
+      [PartitionKey]
+      public string Id
+      {
+         get;
+         set;
+      }
+
+      [RowKey]
+      public string Name
+      {
+         get;
+         set;
+      }
+
+      [ETag]
+      public string ETag
+      {
+         get;
+         set;
+      }
+
+      public int Age
+      {
+         get;
+         set;
+      }
+   }
+}
